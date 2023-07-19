fe4998ec7295f00bcbde15b9d0cbb0d811426c0b
atulsriv <atul.srivastava@bloxroute.com>
2023-07-19 17:33:19 -0400

    add two sum

---
+++ solutions/_1.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # seen is storing index to value at that index
+        seen = {}
+        seen[nums[0]] = 0
+
+        for i in range(1, len(nums)):
+            if target - nums[i] in seen:
+                return seen[target - nums[i]], i
+            elif nums[i] not in seen:
+                seen[nums[i]] = i
+
+        return []
+
